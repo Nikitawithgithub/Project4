154dab7142b470826acd37c412840ab399bc33ea
Ubuntu <ubuntu@ip-172-31-2-227.ap-southeast-2.compute.internal>
2023-06-07 12:28:44 +0000

    factorial

---
+++ factorial.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\nScript to find the factorial for the set of numbers\n"
+
+num="2 4 5"
+for i in $num
+do
+fact=1
+n=$i
+while [ $i -gt 0 ]
+do
+fact=`expr $fact \* $i`
+i=`expr $i - 1`
+done
+echo -e "The factorial of $n is: $fact\n"
+done
