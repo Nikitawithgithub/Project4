18d2b72e730d2eb8c36366c3425e60e85253de5c
Nikitawithgithub <134151937+Nikitawithgithub@users.noreply.github.com>
2023-06-14 04:29:11 +0100

    Update largestofthree.sh

--- largestofthree.sh
+++ largestofthree.sh
@@ -6,10 +6,10 @@ echo "Enter the first number: $num1"
 read num1
 
 echo "Enter the second number: $num2"
-read $num2
+read num2
 
 echo "Enter the third number: $num3"
-read $num3
+read num3
 
 if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
 then
