cfde4e5ae14f98152b6c8d8d472142f3587ffa1e
Nikitawithgithub <134151937+Nikitawithgithub@users.noreply.github.com>
2023-06-14 04:20:04 +0100

    Update largestofthree.sh

--- largestofthree.sh
+++ largestofthree.sh
@@ -2,13 +2,13 @@
 
 echo -e "\nScript to check the largest of three numbers\n"
 
-echo "Enter the first number: "
+echo "Enter the first number: $num1"
 read $num1
 
-echo "Enter the second number: "
+echo "Enter the second number: $num2"
 read $num2
 
-echo "Enter the third number: "
+echo "Enter the third number: $num3"
 read $num3
 
 if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
